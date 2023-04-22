9cd49d518ded484f54f9ea1c3617b0b3e72249d3
Ellis Donovan <ellisgdonovan@gmail.com>
2023-04-22 16:02:12 +0100

    bump vers

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const { execSync } = require("child_process");
+const fs = require('fs')
+const { execSync } = require('child_process')
 
-const packageJsonPath = "./package.json";
+const packageJsonPath = './package.json'
 
 if (!fs.existsSync(packageJsonPath)) {
-  console.error("Error: package.json not found in the current directory.");
-  process.exit(1);
+  console.error('Error: package.json not found in the current directory.')
+  process.exit(1)
 }
 
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
-
-try {
-  console.log("Installing @eiiisd/prettier-config...");
-  execSync("npm install @eiiisd/prettier-config", {
-    stdio: "inherit",
-  });
-} catch (error) {
-  console.error("Error: Failed to install @eiiisd/prettier-config.");
-  process.exit(1);
-}
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'))
 
 packageJson.dependencies = {
   ...packageJson.dependencies,
-  "@eiiisd/prettier-config": "latest",
-};
-packageJson.prettier = "@eiiisd/prettier-config";
+  '@eiiisd/prettier-config': 'latest',
+}
 
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-console.log("Successfully added @eiiisd/prettier-config to package.json.");
+packageJson.prettier = '@eiiisd/prettier-config'
+
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
+console.log('Successfully added @eiiisd/prettier-config to package.json.')
